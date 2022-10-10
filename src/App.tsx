1c00e955346d7d1cd0f1ab732f14b3c182896982
OluC94 <coker.olu94@gmail.com>
2022-10-10 15:31:51 +0100

    add Safe Area View to deal with iphone screen design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>React Native Test</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
