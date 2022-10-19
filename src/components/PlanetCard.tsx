15c3326434e4f153b01c43059a593a0fb95122cf
Hard-Luck <tyreemarka@gmail.com>
2022-10-19 13:49:41 +0100

    added pictures to planet cards

--- src/components/PlanetCard.tsx
+++ src/components/PlanetCard.tsx
@@ -1,6 +1,11 @@
+import { Card, Image, ListItem } from "@rneui/base";
 import React, { FC, ReactElement } from "react";
 import { Text, View } from "react-native";
-import { LightYearToMiles } from "../utils/planetDataFormat";
+import {
+  LightYearToMiles,
+  massComparedToEarth,
+} from "../utils/planetDataFormat";
+
 export interface Planet {
   distance_light_year: number;
   host_star_mass: number;
@@ -15,26 +20,64 @@ export interface Planet {
 export interface PlanetProps {
   planet: Planet;
 }
+
+export interface ImageReqs extends Object {
+  Mercury: any;
+  Venus: any;
+  Mars: any;
+  Jupiter: any;
+  Saturn: any;
+  Uranus: any;
+  Neptune: any;
+  Pluto: any;
+}
 const styleCard = {
   margin: 5,
   backgroundColor: "#aa00aa22",
   borderRadius: 5,
   padding: 5,
 };
+const planetRequires: ImageReqs = {
+  Mercury: require(`../images/mercury.jpg`),
+  Venus: require(`../images/venus.jpg`),
+  Mars: require(`../images/mars.jpg`),
+  Jupiter: require(`../images/jupiter.jpg`),
+  Saturn: require(`../images/saturn.jpg`),
+  Uranus: require(`../images/uranus.jpg`),
+  Neptune: require(`../images/neptune.jpg`),
+  Pluto: require(`../images/pluto.jpg`),
+};
+type PlanetKey = keyof typeof planetRequires;
 
 const PlanetCard: FC<PlanetProps> = (props): ReactElement => {
   const { planet } = props;
+  const location = planetRequires[planet.name as PlanetKey];
   return (
-    <View style={styleCard}>
-      <Text style={{ fontSize: 20 }}>{planet.name}</Text>
-      <Text>
-        1 year on {planet.name} is {planet.period} Earth days
-      </Text>
-      <Text>
-        {planet.name} is {LightYearToMiles(planet.distance_light_year)} miles
-        from Earth.
-      </Text>
-    </View>
+    <Card containerStyle={styleCard}>
+      <View style={{ alignItems: "center" }}>
+        <Card.Title>{planet.name}</Card.Title>
+        {location && (
+          <Image
+            style={{ width: 100, height: 100, borderRadius: 100 }}
+            source={location}
+          />
+        )}
+      </View>
+      <Card.Divider />
+      <View>
+        <ListItem>
+          <Text>
+            {LightYearToMiles(planet.distance_light_year)} miles away from earth
+          </Text>
+        </ListItem>
+        <ListItem>
+          <Text>{massComparedToEarth(planet.mass)} mass of the earth</Text>
+        </ListItem>
+        <ListItem>
+          <Text>{planet.temperature} Celcius</Text>
+        </ListItem>
+      </View>
+    </Card>
   );
 };
 
--- src/utils/planetDataFormat.ts
+++ src/utils/planetDataFormat.ts
@@ -1,3 +1,8 @@
-export const LightYearToMiles = (lightyear: number): number => {
-  return lightyear * 5878600000000;
+export const LightYearToMiles = (lightyear: number): string => {
+  return (lightyear * 5878600000000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+export const massComparedToEarth = (mass: number): string => {
+  return (mass * 5.9 / 1.8565 * 10 ** 2).toFixed(2).toString()
+
+}
