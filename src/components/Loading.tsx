1cacb59de4584fa60450939078fee396fcdc3c9c
Hard-Luck <tyreemarka@gmail.com>
2022-10-20 09:46:22 +0100

    loading screen update

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,23 @@
 import React, { FC } from "react";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 
 export const Loading: FC = () => {
-  return <ActivityIndicator size="large" color="#835188" />;
+  return (
+    <View style={[styles.container, styles.horizontal]}>
+      <ActivityIndicator size="large" color="#835188" />
+      <Text>Loading...</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+  },
+});
